0327c7aa7c2422e61ba5d48bdac96db526bf9cf3
eorb0717 <eorb0717@naver.com>
2024-01-17 17:10:23 +0900

    토큰 수정

--- src/axios/api.js
+++ src/axios/api.js
@@ -1,7 +1,19 @@
 import axios from "axios";
-
+import Cookies from "js-cookie";
 const BASE_URL = "https://www.quizess.store/api";
 
 export const api = axios.create({
   baseURL: BASE_URL,
+});
+
+api.interceptors.request.use((api) => {
+  const access_token = Cookies.get("access_token");
+  const refresh_token = Cookies.get("refresh_token");
+
+  if (api.url === "/api/sign-in") {
+    api.headers.Refresh = refresh_token;
+  } else {
+    api.headers.Authorization = access_token;
+  }
+  return api;
 });
